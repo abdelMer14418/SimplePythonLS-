cebcc70e465484b074ff79d17b9b29f5014ec576
abdelhak <abdelhakmerabtene@gmail.com>
2023-12-01 17:47:31 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import os
 
 
 def list_files():
-    
+    # Obtenir la liste des fichiers et répertoires
+    # dans le répertoire actuel
     files = os.listdir()
 
+    # Afficher chaque fichier ou répertoire
     for file in files:
         print(file)
 
@@ -14,9 +16,11 @@ class Lister:
         pass
 
     def list_files(self):
-       
+        # Obtenir la liste des fichiers et répertoires
+        # dans le répertoire actuel
         files = os.listdir()
 
+        # Afficher chaque fichier ou répertoire
         for file in files:
             print(file)
 
